6aed8df7c37b924443dea742acaed5533f8b1731
Jose Guevara <94gjose@gmail.com>
2022-02-02 19:21:50 -0400

    feat: add telegram icon

---
+++ src/assets/icons/TelegramIcon.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const TelegramIcon = () => {
+  return (
+    <svg
+      width='24'
+      height='24'
+      strokeWidth='1.5'
+      viewBox='0 0 24 24'
+      fill='none'
+      xmlns='http://www.w3.org/2000/svg'
+      className='fill-current hover:scale-110 transition-all hover:border-b-2 hover:border-white'
+      stroke='currentColor'
+    >
+      <path
+        d='M21 5L2 12.5L9 13.5M21 5L18.5 20L9 13.5M21 5L9 13.5M9 13.5V19L12.2488 15.7229'
+        strokeLinecap='round'
+        strokeLinejoin='round'
+      />
+    </svg>
+  )
+}
+
+export default TelegramIcon
--- src/components/navbar/NavBar.tsx
+++ src/components/navbar/NavBar.tsx
@@ -1,34 +1,65 @@
 import React, { useState } from 'react'
+import TelegramIcon from '../../assets/icons/TelegramIcon'
 
 type Props = {
-  children: React.ReactNode;
-  logo: any;
+  children: React.ReactNode
+  logo: any
 }
 
 const NavBar = ({ children, logo }: Props) => {
   const [isOpen, setIsOpen] = useState<boolean>(false)
+  const [writingMessage, setWritingMessage] = useState<boolean>(false)
 
   return (
     <header className='sticky top-0 z-40 w-full backdrop-blur flex-none lg:z-50 dark:border-slate-50/[0.06] dark:bg-slate-900/75 left-0 sm:flex sm:justify-between sm:py-3 sm:px-4 sm:items-center border-b'>
       <div className='flex justify-between items-center py-3 px-4 sm:p-0'>
-        <div>
+        <div className='flex items-center gap-4'>
           {logo}
+          <div className='text-white'>
+            <button onClick={() => setWritingMessage(!writingMessage)}>
+              <TelegramIcon />
+            </button>
+          </div>
         </div>
 
         <div className='sm:hidden'>
-         <button type='button'
-           className='text-gray-500 hover:text-white focus:text-white focus:outline-none'
-           onClick={() => setIsOpen(!isOpen)}
-         >
-          <svg className="w-6 h-6 fill-current" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-            <path className={isOpen ? 'block' : 'hidden'} strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" /> :
-            <path className={isOpen ? 'hidden' : 'block'} strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
-           </svg>
-         </button>
-       </div>
+          <button
+            type='button'
+            className='text-gray-500 hover:text-white focus:text-white focus:outline-none'
+            onClick={() => setIsOpen(!isOpen)}
+          >
+            <svg
+              className='w-6 h-6 fill-current'
+              fill='none'
+              stroke='currentColor'
+              viewBox='0 0 24 24'
+              xmlns='http://www.w3.org/2000/svg'
+            >
+              <path
+                className={isOpen ? 'block' : 'hidden'}
+                strokeLinecap='round'
+                strokeLinejoin='round'
+                strokeWidth={2}
+                d='M6 18L18 6M6 6l12 12'
+              />{' '}
+              :
+              <path
+                className={isOpen ? 'hidden' : 'block'}
+                strokeLinecap='round'
+                strokeLinejoin='round'
+                strokeWidth={2}
+                d='M4 6h16M4 12h16M4 18h16'
+              />
+            </svg>
+          </button>
+        </div>
       </div>
 
-      <div className={`px-2 pt-1 pb-4 ${isOpen ? 'block' : 'hidden'} sm:flex sm:gap-2 sm:p-0`}>
+      <div
+        className={`px-2 pt-1 pb-4 ${
+          isOpen ? 'block' : 'hidden'
+        } sm:flex sm:gap-2 sm:p-0`}
+      >
         {children}
       </div>
     </header>
